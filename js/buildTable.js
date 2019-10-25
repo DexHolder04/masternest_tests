51510ea30ae849a6fa277920daa3693eae38dde4
DexHolder04 <53274549+DexHolder04@users.noreply.github.com>
2019-10-25 00:49:22 -0600

    Mobile Navbar Revamp
    ° Responsive ul li width
    ° FA icons for home and GPX on mobile
    ° Shenanigans

--- js/buildTable.js
+++ js/buildTable.js
@@ -1,33 +1,37 @@
-// Builds the HTML Table out of myList json data from Ivy restful service.
+// Builds the HTML Table out of nestData JSON data
  function buildHtmlTable() {
-     var columns = addAllColumnHeaders(myList);
+     var columns = addAllColumnHeaders(nestData);
      var body$ = $('<tbody/>');
  
-     for (var i = 0 ; i < myList.length ; i++) {
-         var row$ = $('<tr/>');
+     for (var i = 0 ; i < nestData.length ; i++) {
+         var rows = $('<tr/>');
          for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
-             var cellValue = myList[i][columns[colIndex]];
+             var cellValue = nestData[i][columns[colIndex]];
  
              if (cellValue == null) { cellValue = ""; }
  
-             row$.append($('<td/>').html(cellValue));
+             rows.append($('<td/>').html(cellValue));
+             
          }
-        body$.append(row$);
+        body$.append(rows);
+        $('tbody > tr').filter(
+                        function(){
+                                    return $(this).find('td').text().trim().length === 0;
+                                    }).remove();
         $("#nestchart").append(body$);
      }
  }
  
- // Adds a header row to the table and returns the set of columns.
- // Need to do union of keys from all records as some records may not contain
- // all records
- function addAllColumnHeaders(myList)
+ // Adds a header row to the table and returns the set of columns
+ // Need to do union of keys from all records as some records may not contain all elements
+ function addAllColumnHeaders(nestData)
  {
      var columnSet = [];
      var header$ = $('<thead/>');
      var headerTr$ = $('<tr/>');
  
-     for (var i = 0 ; i < myList.length ; i++) {
-         var rowHash = myList[i];
+     for (var i = 0 ; i < nestData.length ; i++) {
+         var rowHash = nestData[i];
          for (var key in rowHash) {
              if ($.inArray(key, columnSet) == -1){
                  columnSet.push(key);
@@ -41,6 +45,7 @@
      return columnSet;
  }
 
+// Calls for clipboard.js
 var clipboard = new ClipboardJS('.btn');
 
 clipboard.on('success', function(e) {
@@ -51,3 +56,23 @@ clipboard.on('error', function(e) {
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
 });
+
+// Navbar minimizing
+function checkRes() {
+    
+    if (window.innerWidth <= 490) {
+        document.getElementById("home").innerHTML = `<i class="fas fa-home faNavbar"></i>`;
+        document.getElementById("gen1").innerHTML = "I";
+        document.getElementById('gen2').innerHTML = "II";
+        document.getElementById('gen3').innerHTML = "III";
+        document.getElementById('gen4').innerHTML = "IV";
+        document.getElementById('gen5').innerHTML = "V";
+        document.getElementById("gpx").innerHTML = `<i class="far fa-map faNavbar"></i>`;
+        
+    } else {
+        
+        return;
+        
+    }
+    
+};
